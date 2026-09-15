a5bd493912f26b2b8d0206a16c8adcaa61a8515e
Big Brother <angsokhey11@gmail.com>
2026-09-15 20:10:15 +0700

    Add grouped no-scroll header for receivable mobile payment table

--- mobile-receivable-table-patch-v3.js
+++ mobile-receivable-table-patch-v3.js
@@ -13,12 +13,21 @@ function apply(){
     link.rel='stylesheet';
     doc.head.appendChild(link);
   }
-  link.href='mobile-receivable-payment-table.css?v=20260915-4';
+  link.href='mobile-receivable-payment-table.css?v=20260915-5';
+
+  const wrap=doc.querySelector('.tablewrap');
+  const table=wrap?.querySelector('table');
+  if(wrap&&table&&!wrap.querySelector('.bb-rph-grid-head')){
+    const head=doc.createElement('div');
+    head.className='bb-rph-grid-head';
+    head.innerHTML='<div>Payment</div><div>Date</div><div>Invoice / Customer</div><div>Location / Sales</div><div>Method / Bank</div><div>Txn / Amount</div><div>Entered / Approved</div>';
+    wrap.insertBefore(head,table);
+  }
 }
 frame.addEventListener('load',function(){
   setTimeout(apply,80);
   setTimeout(apply,300);
   setTimeout(apply,900);
 });
-setTimeout(apply,1400);
+setInterval(apply,1200);
 })();
